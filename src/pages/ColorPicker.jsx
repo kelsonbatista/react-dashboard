09d76970656eff31aa79f9685378831a2a68a685
kelsonbatista <kelsonbatista@gmail.com>
2023-04-13 18:41:36 -0300

    feat: added color picker app

--- src/pages/ColorPicker.jsx
+++ src/pages/ColorPicker.jsx
@@ -1,8 +1,47 @@
-import React from 'react'
+import React from 'react';
+import { ColorPickerComponent } from '@syncfusion/ej2-react-inputs';
+import { Header } from '../components';
+
+const change = (args) => {
+  document.getElementById('preview').style.backgroundColor = args.currentValue.hex;
+}
 
 const ColorPicker = () => {
   return (
-    <div>ColorPicker</div>
+    <div className='m-2 md:m-10 p-2 md:p-10 bg-white rounded-3xl'>
+      <Header category='App' title='Color Picker' />
+      <div className='text-center'>
+        <div id='preview' />
+        <div className='flex justify-center items-center gap-40 flex-wrap'>
+          <div>
+            <p className='text-2xl font-semibold mt-2 mb-4'>
+              Inline Pallete
+            </p>
+            <ColorPickerComponent 
+              id='inline-palette'
+              mode='Palette'
+              modeSwitcher={false}
+              inline={true}
+              showButtons={false}
+              change={change}
+            />
+          </div>
+          <div>
+            <p className='text-2xl font-semibold mt-2 mb-4'>
+              Inline Picker
+            </p>
+            <ColorPickerComponent 
+              id='inline-picker'
+              mode='Picker'
+              modeSwitcher={false}
+              inline={true}
+              showButtons={false}
+              change={change}
+            />
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
