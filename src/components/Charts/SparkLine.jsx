8b620cdd8badb968ab51ae4b23f0cdcd961ddca8
kelsonbatista <kelsonbatista@gmail.com>
2023-04-13 20:54:10 -0300

    fix: sparkline changed to class component

--- src/components/Charts/SparkLine.jsx
+++ src/components/Charts/SparkLine.jsx
@@ -1,31 +1,35 @@
 import React from 'react';
 import { SparklineComponent, Inject, SparklineTooltip } from '@syncfusion/ej2-react-charts';
 
-const SparkLine = ({ id, height, width, color, data, type, currentColor }) => {
-  return (
-    <SparklineComponent 
-      id={ id }
-      height={ height }
-      width={ width }
-      type={ type }
-      lineWidth={ 1 }
-      valueType='Numeric'
-      fill={ color }
-      border={{ color: currentColor, width: 2 }}
-      dataSource={ data }
-      xName='xval'
-      yName='yval'  
-      tooltipSettings={{
-        visible: true,
-        format: '${xval} : ${yval}',
-        trackLineSettings: {
+class SparkLine extends React.PureComponent {  
+  render() {
+    const { id, height, width, type, color, currentColor, data } = this.props;
+    
+    return (
+      <SparklineComponent 
+        id={ id }
+        height={ height }
+        width={ width }
+        type={ type }
+        lineWidth={ 1 }
+        valueType='Numeric'
+        fill={ color }
+        border={{ color: currentColor, width: 2 }}
+        dataSource={ data }
+        xName='xval'
+        yName='yval'  
+        tooltipSettings={{
           visible: true,
-        }
-      }}
-    >
-      <Inject services={[SparklineTooltip]} />
-    </SparklineComponent>
-  )
+          format: '${xval} : ${yval}',
+          trackLineSettings: {
+            visible: true,
+          }
+        }}
+      >
+        <Inject services={[SparklineTooltip]} />
+      </SparklineComponent>
+    )
+  }
 }
 
 export default SparkLine;
