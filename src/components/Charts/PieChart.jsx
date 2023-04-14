9587834ff6e486505fb611879a956b12c21233aa
kelsonbatista <kelsonbatista@gmail.com>
2023-04-14 00:34:18 -0300

    feat: added pie chart

--- src/components/Charts/PieChart.jsx
+++ src/components/Charts/PieChart.jsx
@@ -1,8 +1,46 @@
-import React from 'react'
+import React from 'react';
+import { AccumulationChartComponent, AccumulationSeriesCollectionDirective, AccumulationSeriesDirective, AccumulationLegend, PieSeries, AccumulationDataLabel, Inject, AccumulationTooltip } from '@syncfusion/ej2-react-charts';
+import { pieChartData } from '../../data/dummy';
+import { useStateContext } from '../../contexts/ContextProvider';
 
 const PieChart = () => {
+  const { currentMode } = useStateContext();
+
   return (
-    <div>PieChart</div>
+    <AccumulationChartComponent
+      id='pie-chart'
+      height='420px'
+      legendSettings={{ visible: true, background: 'white' }}
+      chartArea={{ border: { width: 0 } }}
+      tooltip={{ enable: true }}
+      background={ currentMode === 'Dark' ? '#33373E' : '#fff' }
+    >
+      <Inject services={[AccumulationLegend, PieSeries, AccumulationDataLabel, AccumulationTooltip]} />
+      <AccumulationSeriesCollectionDirective>
+        <AccumulationSeriesDirective
+          name="Sale"
+          dataSource={pieChartData}
+          xName="x"
+          yName="y"
+          innerRadius="40%"
+          startAngle={0}
+          endAngle={360}
+          radius="70%"
+          explode
+          explodeOffset="10%"
+          explodeIndex={2}
+          dataLabel={{
+            visible: true,
+            name: 'text',
+            position: 'Inside',
+            font: {
+              fontWeight: '600',
+              color: '#fff',
+            },
+          }}
+        />
+      </AccumulationSeriesCollectionDirective>
+    </AccumulationChartComponent>
   )
 }
 
--- src/pages/Charts/Pie.jsx
+++ src/pages/Charts/Pie.jsx
@@ -1,8 +1,14 @@
-import React from 'react'
+import React from 'react';
+import { Header, PieChart } from '../../components';
 
 const Pie = () => {
   return (
-    <div>Pie</div>
+    <div className='m-4 md:mt-10 mt-24 p-10 bg-white dark:bg-secondary-dark-bg rounded-3xl'>
+      <Header category='Pie' title='Project Cost Breakdown' />
+      <div className='w-full'>
+        <PieChart />
+      </div>
+    </div>
   )
 }
 
