1e6793dd009795939d1660ec4f944c2ed4c2d1a6
kelsonbatista <kelsonbatista@gmail.com>
2023-04-14 00:34:28 -0300

    feat: added financial data

--- src/pages/Charts/Financial.jsx
+++ src/pages/Charts/Financial.jsx
@@ -1,9 +1,48 @@
-import React from 'react'
+import React from 'react';
+import { ChartComponent, SeriesCollectionDirective, SeriesDirective, Inject, HiloSeries, Tooltip, DateTime, Zoom, Logarithmic, Crosshair } from '@syncfusion/ej2-react-charts';
+import { financialChartData, FinancialPrimaryXAxis, FinancialPrimaryYAxis } from '../../data/dummy';
+import { useStateContext } from '../../contexts/ContextProvider';
+import { Header } from '../../components';
 
-const Financial = () => {
+const PieChart = () => {
+  const { currentMode } = useStateContext();
+  const date1 = new Date('2017, 1, 1');
+
+  function filterValue(value) {
+    if (value.x >= date1) {
+      // eslint-disable-next-line no-sequences
+      return value.x, value.high, value.low;
+    }
+  }
+  const returnValue = financialChartData.filter(filterValue);
+
   return (
-    <div>Financial</div>
+    <div className='m-4 md:mt-10 mt-24 p-10 bg-white dark:bg-secondary-dark-bg rounded-3xl'>
+      <Header category='Financial' title='Apple Historical' />
+      <ChartComponent
+        id='financial-chart'
+        height='420px'
+        primaryXAxis={ FinancialPrimaryXAxis }
+        primaryYAxis={ FinancialPrimaryYAxis }
+        chartArea={{ border: { width: 0 } }}
+        tooltip={{ enable: true, shared: true }}
+        background={ currentMode === 'Dark' ? '#33373E' : '#fff' }
+      >
+        <Inject services={[HiloSeries, Tooltip, DateTime, Logarithmic, Crosshair, Zoom]} />
+        <SeriesCollectionDirective>
+          <SeriesDirective
+            dataSource={returnValue}
+            xName="x"
+            yName="low"
+            name="Apple Inc"
+            type="Hilo"
+            low="low"
+            high="high"
+          />
+        </SeriesCollectionDirective>
+      </ChartComponent>
+    </div>
   )
 }
 
-export default Financial
+export default PieChart
